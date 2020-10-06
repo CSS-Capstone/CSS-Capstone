aa81f5084f33689827b9e7c0b9f25a447bb17caa
jhpp114 <jhpp114@uw.edu>
2020-10-06 12:11:23 -0700

    set up dummy url routes, this will be seperate by using router method later

--- index.js
+++ index.js
@@ -6,6 +6,85 @@ app.get('/', (req,res) => {
     res.send('Hello');
 });
 
+app.get('/faq', (req, res) => {
+    res.send('hello: faq')
+});
+
+app.get('/about', (req, res) => {
+    res.send('hello: about');
+});
+
+// =============================
+// Things may change============
+// =============================
+
+// =============================
+// Basic Hotel Finder===========
+// =============================
+
+app.get('/hotel/search', (req, res) => {
+    res.send('hello: main Search');
+});
+
+app.get('/hotel/search/:id', (req, res) => {
+    res.send('hello: Search Hotel Detail');
+});
+
+app.get('/hotel/search/:id/book', (req, res) => {
+    res.send('hello: Searched Hotel Booking');
+});
+
+app.get('/hotel/search/:id/book', (req, res) => {
+    res.send('hello: Searched Hotel Booking Confirmation');
+});
+
+// =============================
+// Register && Login============
+// =============================
+app.get('/login', (req, res) => {
+    res.send('hello: Login page');
+});
+
+app.get('/register', (req, res) => {
+    res.send('hello: Register page');
+});
+
+
+// =============================
+// Basic Hotel CRUD=============
+// =============================
+app.get('/account', (req, res) => {
+    res.send('hello: user account page where user sees there posts and make actions post, edit, and delete');
+});
+
+app.get('/account/new', (req, res) => {
+    res.send('hello: Posting new Hotel');
+});
+
+app.get('/account/:id', (req, res) => {
+    res.send('hello: detail of the user posted hotel');
+});
+
+app.get('/account/:id/edit', (req, res) => {
+    res.send('hello: render edit page of posted hotel');
+});
+
+// put method after edit the hotel
+app.put('/account/:id', (req, res) => {
+    res.send('hello: I am the put page of after edit (save edited data happends here');
+});
+
+// redner delete hotel page
+app.get('/account/:id/delete', (req, res) => {
+    res.send('hello: I am deleting page for hotel');
+})
+
+// delete action method
+app.delete('/account/', (req, res) => {
+    res.send('hello: I am the action method after user clicked delete');
+});
+
+
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
