d4ade44218c32d2ef578307595ce708c7088351a
jhpp114 <jhpp114@uw.edu>
2020-10-05 17:42:36 -0700

    change render to send

--- index.js
+++ index.js
@@ -3,19 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.render('Hello');
-});
-
-app.get('/', (req,res) => {
-    res.render('Hello');
-});
-
-app.get('/', (req,res) => {
-    res.render('Hello');
-});
-
-app.get('/', (req,res) => {
-    res.render('Hello');
+    res.send('Hello');
 });
 
 app.listen(3000, () => {
