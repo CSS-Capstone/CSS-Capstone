dd3247ab13c9c595ab40bbd14c471ee06d6d5259
jhpp114 <jhpp114@uw.edu>
2020-10-07 16:54:11 -0700

    udpate port

--- index.js
+++ index.js
@@ -121,5 +121,5 @@ app.delete('/account/', (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("Server is listening on port 3000");
+    console.log(`Server is listening on port ${port}`);
 });
