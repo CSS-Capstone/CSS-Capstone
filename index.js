19e77a3e8a354d9f829c217b76b0d7ad0742461f
jhpp114 <jhpp114@uw.edu>
2020-10-07 14:20:09 -0700

    go for html for now due to health check path set up on aws

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 // ===============================================
 // ============ Database connection ==============
 // ===============================================
@@ -38,7 +38,7 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req,res) => {
-    res.render('index');
+    res.render('index.html');
 });
 
 app.get('/faq', (req, res) => {
