89cd73ca64bf8c939531127bd16cb4f7c854e051
tarcisiusdaniel <tarcih@uw.edu>
2020-10-19 15:25:58 -0700

    modal login

---
+++ public/js/ModalLogin/ModalLogin.js
@@ -0,0 +1,8 @@
+function openLoginForm() {
+    document.body.classList.add("showLoginForm");
+    console.log("connected");
+}
+
+function closeLoginForm() {
+    document.body.classList.remove("showLoginForm");
+}
