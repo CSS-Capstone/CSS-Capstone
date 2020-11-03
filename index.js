e916c022cd20e1d411f7f836a40290e33b9c0831
jhpp114 <jhpp114@uw.edu>
2020-11-02 16:30:47 -0800

    fixing bug on post hotel

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require('./passport/passport-facebook-setup');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static("stylesheets"));
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
