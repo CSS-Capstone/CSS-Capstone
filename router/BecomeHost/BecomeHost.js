732cfb6f9b85d2a87ae3f5c193365fe4c45511c4
jhpp114 <jhpp114@uw.edu>
2020-11-16 00:06:38 -0800

    added thank you for posting

--- router/BecomeHost/BecomeHost.js
+++ router/BecomeHost/BecomeHost.js
@@ -8,22 +8,23 @@ const dotenv = require('dotenv');
 const db = require('../../utilities/db.js');
 const uuid = require('uuid');
 const multer = require('../../utilities/multer.js');
+const nodemailer = require('nodemailer');
 ///////////////////////////////////
 // multer to AWS S3 upload logics
 ///////////////////////////////////
 const s3 = require('../../utilities/s3');
-
-// const storage = multer.memoryStorage({
-//     destination: function(req, file, callback) {
-//         callback(null, '');
-//     }
-// });
-// // mulitple
-// const upload_multiple = multer({storage:storage}).array('hotelImages');
+////////////////////////////////////
+/////////// NODE MAILER ////////////
+let transporter = nodemailer.createTransport({
+    service: "hotmail",
+    auth: {
+      user: "hotelfinder114@outlook.com",
+      pass: "Hotel0505114"
+    }
+});
 
 router.get('/become-host', authMW.isLoggedIn, (req, res) => {
     res.render('pages/becomeHost/becomeHostPolicy');
-    //res.send('hello host');
 });
 
 router.get('/become-host/postHotel/new', authMW.isLoggedIn, (req, res) => {
@@ -83,104 +84,102 @@ router.get('/become/postHotelImage', authMW.isLoggedIn, (req, res) => {
 });
 
 router.post('/become-host/postHotelImage', multer.upload_multiple, (req, res) => {
-    // console.log(req.files);
-    const postedHotelID = req.body.postedHotelId;
+    let postedHotelID = req.body.postedHotelId;
     let currentPostingUser = req.session.user;
     let currentPostingUserID = currentPostingUser.user_id;
-    console.log(currentPostingUser);
     let hotelImageData = req.files;
-    console.log(hotelImageData);
     // DB CONNECT
-    for (let i = 0; i < hotelImageData.length; i++) {
-        let eachImageData = hotelImageData[i];
-        // console.log(eachImageData.mimetype);
-        if (eachImageData.mimetype == 'image/jpg' || eachImageData.mimetype == 'image/jpeg' || eachImageData.mimetype == 'image/png') {
-            console.log("is it here?");
-            // console.log(eachImageData.originalname.split("."));
-            let splitByDataType = eachImageData.originalname.split(".");
-            console.log(splitByDataType[0]);
-            console.log(splitByDataType[1]);
-            let fileName = uuid.v5(splitByDataType[0], process.env.SEED_KEY);
-            console.log(fileName);
-            let fullHotelImageName = currentPostingUserID + '_'  + fileName + '.' + splitByDataType[1];
-            console.log(fullHotelImageName);
-
-            // PARAMETER FOR UPLOAD IN S3 BUCKET
-            let params = {
-                Bucket: process.env.AWS_HOTEL_BUCKET_NAME
-            ,   Key: `${fullHotelImageName}`
-            ,   Body: eachImageData.buffer
-            };
-
-            s3.s3.upload(params, (error, data) => {
-                if (error) {
-                    res.status(500).send(error);
-                }
-                
-                console.log("============== AWS S3 ACCESS ==========");
-                const tempUserId = currentPostingUser.user_id;
-                let hotelImageID = data.key;
-                let fileLocation = data.Location;
-                console.log(hotelImageID);
-                console.log(fileLocation);
-                const insertHotelImageQuery = "INSERT INTO `css-capstone`.USER_HOTEL_IMAGE SET ?";
-                db.query(insertHotelImageQuery, 
-                    {
-                        // this will be change into req.session.user later
-                        user_id: tempUserId
-                    ,   hotel_img_id: fullHotelImageName
-                    ,   hotel_img: fileLocation
-                    ,   hotel_id: postedHotelID
-                    }, (err, result) => {
-                        // callback function
-                        if (err) {
-                            console.log(err);
-                            console.log("Error on Hotel Image Database");
-                            throw err;
-                        }
-                        console.log("Successfully added in the Hotel Image Database");
-                    }
-                )
-            });
-            //res.render('pages/hostHotel/hotelPostImage');
-        } else {
-            console.log("It should be here....");
-            
+    const HOTEL_SELECT_QUERY = `SELECT * FROM HOTEL WHERE hotel_id=?`;
+    db.query(HOTEL_SELECT_QUERY, postedHotelID, async (error, hotelResult) => {
+        if (error) {
+            console.log("ERROR: HOTEL DATA Retriving from DB FAIL");
         }
-        
-    }
-    req.session.hotelPostId = postedHotelID;
-    res.redirect('/become-host/postHotelThankyou');
+        for (let i = 0; i < hotelImageData.length; i++) {
+            let eachImageData = hotelImageData[i];
+            // console.log(eachImageData.mimetype);
+            if (eachImageData.mimetype == 'image/jpg' || eachImageData.mimetype == 'image/jpeg' || eachImageData.mimetype == 'image/png') {
+                // console.log("is it here?");
+                // console.log(eachImageData.originalname.split("."));
+                let splitByDataType = eachImageData.originalname.split(".");
+                let fileName = uuid.v5(splitByDataType[0], process.env.SEED_KEY);
+                let fullHotelImageName = postedHotelID + '_' + currentPostingUserID + '_'  + fileName + '.' + splitByDataType[1];    
+                // PARAMETER FOR UPLOAD IN S3 BUCKET
+                let params = {
+                    Bucket: process.env.AWS_HOTEL_BUCKET_NAME
+                ,   Key: `${fullHotelImageName}`
+                ,   Body: eachImageData.buffer
+                };
+                s3.s3.upload(params, (error, data) => {
+                    if (error) {
+                        res.status(500).send(error);
+                    }
+                    console.log("============== AWS S3 ACCESS ==========");
+                    let tempUserId = currentPostingUser.user_id;
+                    let hotelImageID = data.key;
+                    let fileLocation = data.Location;
+                    const insertHotelImageQuery = "INSERT INTO `css-capstone`.USER_HOTEL_IMAGE SET ?";
+                    db.query(insertHotelImageQuery, 
+                        {
+                            // this will be change into req.session.user later
+                            user_id: tempUserId
+                        ,   hotel_img_id: fullHotelImageName
+                        ,   hotel_img: fileLocation
+                        ,   hotel_id: postedHotelID
+                        }, (err, result) => {
+                            // callback function
+                            if (err) {
+                                console.log(err);
+                                console.log("Error on Hotel Image Database");
+                                throw err;
+                            }
+                            console.log("Successfully added in the Hotel Image Database");
+                    });
+                });
+                //res.render('pages/hostHotel/hotelPostImage');
+            } else {
+                console.log("It should be here....");
+            }
+        }
+        req.session.hotelPostId = postedHotelID;
+        res.redirect('/become-host/postHotelThankyou');
+    }); 
 });
 
-router.get('/become-host/postHotelThankyou', (req, res) => {
-    const postedHotelId = req.session.hotelPostId;
-    const tempPostedHotelId = 22;
+router.get('/become-host/postHotelThankyou', async (req, res) => {
+    let postedHotelId = req.session.hotelPostId;
     req.session.hotelPostId = null;
     // Data Variables to pass to render
     let hotelDataObj = '';
-
-    let hotelInformationQuery = `SELECT * FROM HOTEL H WHERE H.hotel_id = ${tempPostedHotelId}`;
-    // let hotelImageRetrieveQuery = `SELECT hotel_img_id
-    // FROM `css-capstone`.HOTEL AS H
-    //     INNER JOIN `css-capstone`.USER_HOTEL_IMAGE AS HI
-    //     ON HI.hotel_id = H.hotel_id`
-    db.query(hotelInformationQuery, function(err, result) {
+    // let imageArray = [];
+    let hotelInformationQuery = `SELECT * FROM HOTEL H WHERE H.hotel_id = ${postedHotelId}`;
+    // let hotelImageRetrieveQuery = `SELECT hotel_img_id FROM USER_HOTEL_IMAGE WHERE hotel_id=${postedHotelId}`;
+    db.query(hotelInformationQuery, async function(err, resultHotel) {
         if (err) {
             console.log(err);
             console.log("Error happend during retrieve hotel for thank you page");
             throw err;
-        } else {
-
-            hotelDataObj = result[0];
-            console.log(hotelDataObj);
-            res.render('pages/hostHotel/hotelPostThankyou', {tempPostedHotelId:tempPostedHotelId,hotelDataObj:hotelDataObj});
-        }
+        } 
+        hotelDataObj = resultHotel[0];
+        console.log("THANK_YOU: Hotel Data");
+        console.log(hotelDataObj);
+        let info = await transporter.sendMail({
+            from: "hotelfinder114@outlook.com", // sender address
+            to: "jhpp114nemo@gmail.com", // list of receivers
+            subject:"Hotel Finder - Thank you for Posting ✔", // Subject line
+            html: `<div><img width="400" height="400" src="cid:successImage"/> <h2>Thank you for posting Your hotel</h2> <p>Your Hotel,<strong>${hotelDataObj.hotel_name}</strong> located in ${hotelDataObj.address} had been posted! </p></div>` // html body
+            ,attachments: [{
+                filename: 'successfull_post.jpg',
+                path: './public/images/successfully_post.jpg',
+                cid: 'successImage' //same cid value as in the html img src
+            }]
+        });
+        // console.log(imageArray);
+        res.render('pages/hostHotel/hotelPostThankyou', 
+            {
+                postedHotelId:postedHotelId
+            ,   hotelDataObj:hotelDataObj
+        });
     });
-
-    // console.log(tempPostedHotelId);
-    // console.log(hotelDataObj);
-    
 });
 
 module.exports = router;
